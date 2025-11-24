9ee5ce402ce302fa38d20a5c3ad9da0c78fbb967
Addel-Perez <addel.perezospino@unicolombo.edu.co>
2025-11-23 22:53:28 -0500

    feat: environments for supabase and firebase setted.

---
+++ src/environments/environment.example.ts
@@ -0,0 +1,16 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "your-api-key",
+    authDomain: "auth-domain",
+    projectId: "project-id",
+    storageBucket: "storage-bucket",
+    messagingSenderId: "messaging-sender-id",
+    appId: "app-id"
+  },
+  supabase: {
+    bucket:'bucket-name',
+    url:'https://...jxn.supabase.co',
+    Key:'your-anon-supabase-key'
+  }
+};
